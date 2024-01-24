c4cfe9e82233d8566b3ef1aa6f1186be73d13924
Johan <becerrajohan6@gmail.com>
2024-01-24 12:56:35 -0500

    conectado a la base de datos

--- router.js
+++ router.js
@@ -4,42 +4,15 @@ const pool = require("./database/db");
 
 router.get("/", async (req, res) => {
   pool.getConnection((err, connection) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send("Error al obtener la conexión");
-    }
-
     connection.query("SELECT * FROM usuarios", (err, results) => {
-      connection.release(); // Liberar la conexión después de la consulta
-      if (err) {
-        console.error(err);
-        return res.status(500).send("Error en la consulta");
-      }
-
-      res.json(results);
+      res.render("index.ejs", { results: results });
     });
   });
 });
 
-router.get("/ping", async (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send("Error al obtener la conexión");
-    }
-
-    connection.query("SELECT NOW()", (err, results) => {
-      connection.release(); // Liberar la conexión después de la consulta
-      if (err) {
-        console.error(err);
-        return res.status(500).send("Error en la consulta");
-      }
-
-      res.json(results[0]);
-    });
-  });
+// manejar error 404
+router.use((req, res) => {
+  res.status(404).render("404.ejs");
 });
-
-// Otras rutas...
 
 module.exports = router;
